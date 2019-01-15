750ef04f179223a112588e7ae0ad3862a68e8754
Jan Kratochvíl <kratochvil.jan@outlook.com>
2019-01-15 10:14:40 +0100

    Adds Office UI Fabric

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
+import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
+
 import "./App.css";
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <div className="App">
-        <h1> Hello, World! </h1>
+        <PrimaryButton
+          text="I'm a cool button, click me" />
       </div>
     );
   }
