f071937f545201e9ca15ffa3b394dc0648d189c4
Jan Kratochvíl <kratochvil.jan@outlook.com>
2019-01-20 11:21:30 +0100

    Working VSS initialization

--- src/App.js
+++ src/App.js
@@ -4,10 +4,23 @@ import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
 import "./App.css";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.showAlert = this.showAlert.bind(this);
+  }
+
+  showAlert() {
+    var context = VSS.getWebContext();
+    console.log(context);
+  }
+
   render() {
     return (
       <div className="App">
         <PrimaryButton
+          onClick={this.showAlert}
           text="I'm a cool button, click me" />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 
-ReactDOM.render(
-    <App />, 
-    document.getElementById("root"));
+console.log("VSS init");
+
+VSS.init({
+    setupModuleLoader: true,
+    explicitNotifyLoaded: true,
+    usePlatformScripts: true,
+    usePlatformStyles: true,
+    moduleLoaderConfig: {
+        paths: {
+            Scripts: 'scripts'
+        }
+    }
+});
+
+VSS.ready(function () {
+    console.log("VSS ready");
+    
+    ReactDOM.render(
+        <App />, 
+        document.getElementById("root"));
+
+    VSS.notifyLoadSucceeded();
+});
