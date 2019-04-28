25f81291f6ddadda949ec88132c12628812dcc1f
Jan Kratochvíl <kratochvil.jan@outlook.com>
2019-04-28 11:04:59 +0200

    Opens links in new tabs, sanitizes description HTML

--- estimate-web/src/components/UserStoryDetail.jsx
+++ estimate-web/src/components/UserStoryDetail.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import sanitizeHtml from "sanitize-html";
 
 import "./UserStoryDetail.less";
+
+const mixInTargetBlankAttributes = (tagName, attribs) => ({
+    tagName: tagName,
+    attribs: {
+        ...attribs,
+        target: "_blank",
+        rel: "noopener noreferrer"
+    }
+});
 
 const UserStoryDetail = props => {
     const { workItem, urlRoot } = props;
 
+    const sanitizedDescription = sanitizeHtml(
+        workItem.description,
+        {
+            transformTags: {
+                a: mixInTargetBlankAttributes
+            }
+        }
+    );
+
     return (
         <div className="user-story-container">
             <h4 style={{ marginLeft: "0", marginBottom: "0" }}>{workItem.title}</h4>
-            <a href={`${urlRoot}${workItem.id}`} className="user-story-title-row">
+            <a href={`${urlRoot}${workItem.id}`} className="user-story-title-row" target="_blank" rel="noopener noreferrer">
                 <span>{`${workItem.workItemType.toLowerCase()} #${workItem.id} by ${workItem.createdBy.displayName}`}</span>
             </a>
 
             <div dangerouslySetInnerHTML={{
-                __html: workItem.description
+                __html: sanitizedDescription
             }}
             />
         </div>
