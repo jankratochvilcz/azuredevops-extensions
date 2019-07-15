1da19877d9e75664edcf0a2ac2c264e6055b2378
Christoph Krenn <c.j.krenn@gmail.com>
2019-07-15 15:11:09 +0200

    Introduce consts for date-ago calculation

--- estimate-web/src/utils/date.js
+++ estimate-web/src/utils/date.js
@@ -1,24 +1,36 @@
-const timeAgo = date => {
-    const seconds = Math.floor((new Date() - date) / 1000);
+const MILLISECONDS_PER_SECOND = 1000;
+const SECONDS_PER_YEAR = 31536000;
+const SECONDS_PER_MONTH = 2592000;
+const SECONDS_PER_WEEK = 604800;
+const SECONDS_PER_DAY = 86400;
+const SECONDS_PER_HOUR = 3600;
+const SECONDS_PER_MINUTE = 60;
 
-    let interval = Math.floor(seconds / 31536000);
+const timeAgo = date => {
+    const seconds = Math.floor((new Date() - date) / MILLISECONDS_PER_SECOND);
+
+    let interval = Math.floor(seconds / SECONDS_PER_YEAR);
 
     if (interval > 1) {
         return `${interval} years ago`;
     }
-    interval = Math.floor(seconds / 2592000);
+    interval = Math.floor(seconds / SECONDS_PER_MONTH);
     if (interval > 1) {
         return `${interval} months ago`;
     }
-    interval = Math.floor(seconds / 86400);
+    interval = Math.floor(seconds / SECONDS_PER_WEEK);
+    if (interval > 1) {
+        return `${interval} weeks ago`;
+    }
+    interval = Math.floor(seconds / SECONDS_PER_DAY);
     if (interval > 1) {
         return `${interval} days ago`;
     }
-    interval = Math.floor(seconds / 3600);
+    interval = Math.floor(seconds / SECONDS_PER_HOUR);
     if (interval > 1) {
         return `${interval} hours ago`;
     }
-    interval = Math.floor(seconds / 60);
+    interval = Math.floor(seconds / SECONDS_PER_MINUTE);
     if (interval > 1) {
         return `${interval} minutes ago`;
     }
