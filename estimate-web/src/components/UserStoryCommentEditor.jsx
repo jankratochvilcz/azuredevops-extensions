38e9b991bc45e132eccac3ecc81af529ddf8b252
Christoph Krenn <c.j.krenn@gmail.com>
2019-07-22 12:32:53 +0200

    Remove extra border on comment editor

--- estimate-web/src/components/UserStoryCommentEditor.jsx
+++ estimate-web/src/components/UserStoryCommentEditor.jsx
@@ -93,7 +93,7 @@ class UserStoryCommentEditor extends Component {
                         {addingComment && <Spinner className="adding-spinner" />}
                     </PrimaryButton>
                 )}
-                {showAddButton && <Label style={{ fontSize: FontSizes.small }}>Ctrl+Alt</Label>}
+                {showAddButton && <Label style={{ fontSize: FontSizes.small }}>Ctrl+Enter</Label>}
             </div>
         );
     }
